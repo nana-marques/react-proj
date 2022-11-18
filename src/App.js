b00d95a62f6cdb6c54c745902fd1b0a0b1965ac8
prazeresmath <matheus.prazeres@hotmail.com>
2022-11-18 16:06:04 -0300

    Merge branch 'master' of https://github.com/nana-marques/react-proj

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ola <code>src/App.js</code> and save to reload.
+          ola <code>src/App.js</code> e save to reload.
         </p>
         <a
           className="App-link"
