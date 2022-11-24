d8930e4865e9b9ca8f011dbae383939258f91265
Ana Marques <ana.marques@crfsp.org.br>
2022-11-24 11:14:20 -0300

    att navbar

--- src/assets/layout/navBar/index.jsx
+++ src/assets/layout/navBar/index.jsx
@@ -3,17 +3,27 @@
 import Styles from './index.module.css'
 //import help
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import logo from '../../img/logo.png'
+import { FaSearch } from 'react-icons/fa';
+import Logo from '../../img/logo.png'
 
 function navBar(){
     return (
         <Router>
             <nav className={Styles.navBar}>
-                <ul>
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/search">Search</Link></li>
-                    <li><Link to="/carrinho">Carrinho</Link></li>
+                <img src={Logo} alt="Logo RadTek" className={Styles.navLogo}/>
+
+                <label htmlFor="search" className={Styles.navLabel}>
+                    <input type="text" id="search" className={Styles.navSearch}/>
+                    <button className={Styles.button}><FaSearch className={Styles.navSearchButton}/></button>
+                    {/* <input type="button" value={FaSearch} className={Styles.navSearchButton}/> */}
+                    {/* <div className={Styles.navButton}></div> */}
+                </label>
+
+
+                <ul className={Styles.navLink}>
+                    <li><Link to="/home" style={{ textDecoration: 'none' }}  className={Styles.navItem}>Home</Link></li>
+                    <li><Link to="/search" style={{ textDecoration: 'none' }}  className={Styles.navItem}>Login</Link></li>
+                    <li><Link to="/carrinho" style={{ textDecoration: 'none' }}  className={Styles.navItem}>Carrinho</Link></li>
                 </ul>
             </nav>
         </Router>
@@ -21,3 +31,4 @@ function navBar(){
 }
 
 export default navBar;
+
