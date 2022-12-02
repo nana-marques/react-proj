b493a3eb7e5ba264f0490f09a08f8c54eefb84b3
Guilherme Cardoso <guiilherme.cardoso4@gmail.com>
2022-12-02 10:36:17 -0300

    Comit

--- src/assets/layout/home/index.jsx
+++ src/assets/layout/home/index.jsx
@@ -5,14 +5,8 @@ import Carrosel from '../carrosel/index2'
 
 function home(){
     return (
-<<<<<<< HEAD
         <div>
            <Carrosel/>
-=======
-        <div className={Styles.home}>
-            ase
-            {/* jkbkbkb */}
->>>>>>> 3f50d9a4ebcb43bc57b2a7eebe7ccdcf2cdd84ec
         </div>
     )
 }
